942ed802958d19eff7712d424b2ef39b22ab8a08
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-06-24 09:49:10 -0400

    updated top module with sign extended and register file
    I connected the output of the instruction memory to the register file and the sign extend

--- microprocesser_files/single_cycle_processor_files/design_files/processor_top.v
+++ microprocesser_files/single_cycle_processor_files/design_files/processor_top.v
@@ -2,6 +2,13 @@ module processor_top(
     input clk,
     input reset_PC,
     input [31:0] PC_D,
+    input WE3,
+    input [4:0] A2,
+    input [4:0] A3,
+    input [31:0] WD3,
+    output [31:0] RD1,
+    output [31:0] RD2,
+    output [31:0] IM_RD_out,
     output [31:0] PC_Q,
     output [31:0] PC_Q_not,
     output [31:0] IM_RD
@@ -9,5 +16,8 @@ module processor_top(
     PC_reg PC(.clk(clk), .reset(reset_PC), .D(PC_D), .Q(PC_Q), .Q_not(PC_Q_not));
     instruction_memory IM(.addr(PC_Q[9:0]), .RD(IM_RD));
 
-    
+    register_file regfile(.clk(clk), .WE3(WE3), .A1(IM_RD[25:21]), .A2(A2), .A3(A3), .WD3(WD3), .RD1(RD1), .RD2(RD2));
+    sign_extend se(.instr(IM_RD[15:0]), .extended_instr(IM_RD_out));
+
+
 endmodule
